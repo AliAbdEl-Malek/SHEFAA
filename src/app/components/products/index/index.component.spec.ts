e91c62479e3486ca99b0935185a43c3730bff285
Mahmoud Talaya <mahmoudtalaya2014@gmail.com>
2021-02-03 00:59:35 +0200

    home and regestration resposive

--- src/app/components/products/index/index.component.spec.ts
+++ src/app/components/products/index/index.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { UserInformationComponent } from './user-information.component';
+import { IndexComponent } from './index.component';
 
-describe('UserInformationComponent', () => {
-  let component: UserInformationComponent;
-  let fixture: ComponentFixture<UserInformationComponent>;
+describe('IndexComponent', () => {
+  let component: IndexComponent;
+  let fixture: ComponentFixture<IndexComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UserInformationComponent ]
+      declarations: [ IndexComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(UserInformationComponent);
+    fixture = TestBed.createComponent(IndexComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
