42c97915db3f773961b32b8ba58d0a6c02bdde4d
Ali Abd El-Malek <aliabdelmalek90@gmail.com>
2021-02-10 00:07:12 +0200

    prescription backend 9/2/2021 no.2

--- routes/prescription.js
+++ routes/prescription.js
@@ -8,7 +8,7 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, './prescription/')
+        cb(null, './prescriptions/')
     },
     filename: function (req, file, cb) {
         cb(null, Date.now() + '_' + file.originalname)
