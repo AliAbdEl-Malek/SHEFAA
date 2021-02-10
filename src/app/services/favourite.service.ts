ad08bd13aaea5c5b83989c485b3d618978373d3d
8ahmedmohamed <ahmedkhan.am8@gmail.com>
2021-02-10 04:25:34 +0200

    10/2/2021 ahmed 2

--- src/app/services/favourite.service.ts
+++ src/app/services/favourite.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { User} from '../models/user';
+import { Product } from '../models/Product';
+import { ApiService } from './api.service'
 
 @Injectable({
   providedIn: 'root'
@@ -6,17 +9,29 @@ import { Injectable } from '@angular/core';
 export class FavouriteService {
 
   list:any[] = [];
-  constructor() { }
-  addToFavourite(product:any) {
-    this.list.push(product);
+  fav:Product[] = [];
+  user:User;
+
+  constructor( private _apiService:ApiService) { }
+
+  addToFavourite(product:any , id:any) {
+    if (this.fav.indexOf(product) === -1){
+      this.fav.push(product);
+      this._apiService.put(`favourite/add/${id}`,this.fav).subscribe();
+      console.log("favourite "+product);
+      this.fav=[];
+    }
+    else{
+      alert("added")
+    }
   }
 
-  getFavourite() {
-    return this.list;
-  }
+  // getFavourite() {
+  //   return this.list;
+  // }
 
   getItemsLength() {
-    return this.list.length;
+    return this.fav.length;
   }
 
   clearFavourite() {
@@ -24,8 +39,15 @@ export class FavouriteService {
     return this.list;
   }
 
-  deleteFavourite(index:number)
-  {
-    this.list.splice(index,1)
+  // deleteFavourite(index:number)
+  // {
+  //   this.list.splice(index,1)
+  // }
+
+  deleteFavourite(userId:any,productId:any,index:number){
+    this._apiService.put(`favourite/delete/${productId}`,{userId}).subscribe();
+
+    console.log("favourite deleted "+userId+"//"+productId);
+    this.fav.splice(index,1);
   }
 }
